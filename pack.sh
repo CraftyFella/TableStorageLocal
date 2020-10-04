38a240b957a72482d25ec364369f843bcd4e43f2
Craftyfella <craftyfella@gmail.com>
2020-10-04 17:26:37 +0100

    Remove dependecy numbers.

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
-NUGETVERSION=0.0.1-alpha
+NUGETVERSION=0.0.2-alpha
 dotnet pack src/AzureTableStorage/AzureTableStorage.fsproj -c Release /p:PackageVersion=$NUGETVERSION
 dotnet nuget push src/AzureTableStorage/bin/Release/FakeAzureTables.$NUGETVERSION.nupkg -k $NUGETKEY -s nuget.org
