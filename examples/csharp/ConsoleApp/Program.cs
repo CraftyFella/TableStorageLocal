3d5a479f351a69ff60ee6c0554eca5ebb64d3bd7
Jon Canning <jon.canning@gmail.com>
2020-10-28 15:19:52 +0000

    make csharp example a standalone project

--- examples/csharp/ConsoleApp/Program.cs
+++ examples/csharp/ConsoleApp/Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using TableStorageLocal;
 using Microsoft.Azure.Cosmos.Table;
+using System.Collections.Generic;
 
 namespace csharp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      var tables = new LocalTables();
-      var client = CloudStorageAccount.Parse(tables.ConnectionString).CreateCloudTableClient();
-      var table = client.GetTableReference("test");
-      table.CreateIfNotExists();
-
-      Console.WriteLine($"ConnectionString is {tables.ConnectionString}");
-      Console.ReadLine();
+        static void Main(string[] args)
+        {
+            var tables = new LocalTables(10002);
+            var client = CloudStorageAccount.Parse(tables.ConnectionString).CreateCloudTableClient();
+            var table = client.GetTableReference("test");
+            table.CreateIfNotExists();
+            var entity = new DynamicTableEntity("PK", "RK", "*", new Dictionary<string, EntityProperty>() { { "Message", EntityProperty.GeneratePropertyForString("Hello, World!") } });
+            table.Execute(TableOperation.Insert(entity));
+            var result = table.Execute(TableOperation.Retrieve("PK", "RK"));
+            Console.WriteLine(((DynamicTableEntity)result.Result).Properties["Message"]);
+            Console.ReadLine();
+        }
     }
-  }
 }
